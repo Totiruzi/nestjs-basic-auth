979247c60b70961e6ba4884df23f59d0eb0952d1
Totiruzi <totiruzi@yahoo.com>
2022-05-23 14:20:58 +0300

    Install joi for validation at runtime

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+
+  const config = app.get(ConfigService)
+  const port = config.get<string>('PORT')
+  await app.listen(port);
 }
 bootstrap();
